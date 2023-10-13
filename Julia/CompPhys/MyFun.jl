54374c23d27ace078809e829d4e1b19745fd3958
Constantin Zitta <constantin.zitta@student.tugraz.at>
2023-10-13 15:17:28 +0200

    integrated lower/upper matrix  in forward/backward sub

--- Julia/CompPhys/MyFun.jl
+++ Julia/CompPhys/MyFun.jl
@@ -22,6 +22,7 @@ end
 
 # forward substitution 
 function ForwardSubstitution!(L, b)
+    L = LowerTriangular(A)
     n = size(b, 1)
     for i in 1:n # loop every row
         for k in 1:i-1 # loop every column except the last one using only indices lower than i
@@ -34,6 +35,7 @@ end
 
 # backwardsubstitution
 function BackwardSubstitution!(U, b)
+    U = UpperTriangular(A)
     n = size(b, 1)
     for i in n:-1:1 # loop every row
         for k in i+1:n # using indices including i and higher than i
