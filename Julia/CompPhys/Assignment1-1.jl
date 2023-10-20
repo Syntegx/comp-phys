f8398a1421807f5c6b1525003a1293c566470215
Constantin Zitta <constantin.zitta@student.tugraz.at>
2023-10-20 15:41:42 +0200

    Fixed Code, now functional

--- Julia/CompPhys/Assignment1-1.jl
+++ Julia/CompPhys/Assignment1-1.jl
@@ -22,6 +22,7 @@ x = collect(1:1:N) .* h;
 # since its a linear differential equation it can be written in the form
 # A y(x) = b where A describes a matrix A, y(x) is a vector containing the elements (y(x_1),...,y(x_N))
 # Matrix a has form a_ii = h^2 k^2 + 2, a_i+1 j = -1 = a_i j+1
+
 zero_N = zeros(N);
 a_ii = k^2 * h^2 + 2;
 A = zeros(N, N) + SymTridiagonal(zero_N .- a_ii, zero_N .+ 1);
@@ -40,77 +41,91 @@ plot(x, y_num);
 plot!(x, DebyeHueckelFunction(x));
 
 # Using LU Decomposition the upper and lower triangular matrices L and U can be found and used to solve the one dimensional Debye-Hueckel equation
-function LUdecomp!(A)
+function LUdecomp(A)
     # first we determine the size nxn of the matrix A
     n = size(A, 1)
-    A = zeros(n, n) + A
+    U = copy(A)
     # here we make sure our matrix contains at least zeros as every element, otherwise the allocation of new values wont work
     for j in 1:n # we loop over every column 1 to n
         # since the first row of our linear system contains only terms of trivial nature we can skip the first row
         for i in j+1:n
-            A[i, j] = A[i, j] / A[j, j] # since all of the values u_1j are known, theres only a single unknown variables l_n1 in the first column
+            U[i, j] = U[i, j] / U[j, j] # since all of the values u_1j are known, theres only a single unknown variables l_n1 in the first column
             for k in j+1:n # after calculating the unknown values l_n1 we can calculate the next unknown variable u_2m since all others are given 
-                A[i, k] = A[i, k] - A[i, j] * A[j, k]
+                U[i, k] = U[i, k] - U[i, j] * U[j, k]
             end
         end
     end
-    return A
+    return U
 end
 
 # forward substitution 
-function ForwardSubstitution!(L, b)
+function ForwardSubstitution(L, b)
     n = size(b, 1)
+    ForwardVector = deepcopy(b);
     for i in 1:n # loop every row
         for k in 1:i-1 # loop every column except the last one using only indices lower than i
-            b[i] = b[i] - L[i, k] * b[k]
+            ForwardVector[i] = ForwardVector[i] - L[i, k] * ForwardVector[k]
         end
     end
-    return b
+    return ForwardVector
 end
 
 # backwardsubstitution
-function BackwardSubstitution!(U, b)
+function BackwardSubstitution(U, b)
     n = size(b, 1)
+    BackwardVector = deepcopy(b);
     for i in n:-1:1 # loop every row
         for k in i+1:n # using indices including i and higher than i
-            b[i] = (b[i] - U[i, k] * b[k])
+            BackwardVector[i] = (BackwardVector[i] - U[i, k] * BackwardVector[k])
         end
-        b[i] = b[i] / U[i, i]
+        BackwardVector[i] = BackwardVector[i] / U[i, i]
     end
-    return b
+    return BackwardVector
 end
 
 # function using all other functions created for solving lin equation systems
 function SolveLinSys(A, b)
-    A = LUdecomp!(A)
-    ForwardSubstitution!(A, b)
-    BackwardSubstitution!(A, b)
-    return b
+    U = LUdecomp(A)
+   ForwardVector = ForwardSubstitution(U, b)
+   BackwardVector = BackwardSubstitution(U, ForwardVector)
+    return BackwardVector
 end
 
-function NegativeTotalCharge_q(y, k, h)
-    q = ((2 * c_0) / k^2) .* (((y[N] - y[N-1]) ./ h) - ((y[2] - y[1]) ./ h))
+# @btime SolveLinSys(A, b)
+
+function FirstDerivative(y, x, h)
+
+    Derivative = (y[x] - y[x-1]) / h
+    return Derivative
+end
+
+function NegativeTotalCharge(y, k, h)
+    q = ((2 * c_0) / k^2) .* (FirstDerivative(y, length(y), h) - FirstDerivative(y, 2, h))
     return q
 end
+NegativeTotalCharge(b, k, h)
 
-Charge_Vec = zeros(N);
-Cap_Vec = zeros(N);
-d = collect(LinRange(1, 100, N))
-y_0 = collect(LinRange(-5, -0.5, N))
-    
-for j in 1:N
-    h = d[j] / N
-    k = sqrt((2 * c_0 * e_0^2) / (epsilon_0 * epsilon_r * k_B * T)) * 1
-    for i in 1:N
-        local y_d = 0;
+
+Total_Its = 100;
+
+Charge_Vec = zeros(Total_Its);
+Cap_Vec = zeros(Total_Its);
+d = collect(LinRange(1, 100, Total_Its))
+y_0 = collect(LinRange(-5, -0.5, Total_Its))
+
+for j in 1:Total_Its
+    local h = d[j] / N
+    local k = sqrt((2 * c_0 * e_0^2) / (epsilon_0 * epsilon_r * k_B * T)) * 1
+    for i in 1:Total_Its
+        local y_d = 0
         local a_ii = k^2 * h^2 + 2
         local A = zeros(N, N) + SymTridiagonal(zero_N .+ a_ii, zero_N .- 1)
         local b = vcat(y_0[i], zeros(N - 2), y_d)
         SolveLinSys(A, b)
-        Charge_Vec[i] = NegativeTotalCharge_q(b, k, h)
+        Charge_Vec[i] = NegativeTotalCharge(b, k, h)
     end
 
-    Cap_Vec[j] = (Charge_Vec[N] - Charge_Vec[1]) ./ (h*N)
+    Cap_Vec[j] = (Charge_Vec[2] - Charge_Vec[1])
 end
-
-plot(d, Charge_Vec)
+plot(y_0, Charge_Vec)
+plot(d, Cap_Vec)
